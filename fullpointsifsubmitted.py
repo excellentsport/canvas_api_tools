6bbb1acc6f584cefac1b2ba4908223a293726592
George Beckham <35553484+excellentsport@users.noreply.github.com>
2023-11-07 11:10:41 -0800

    working script

---
+++ fullpointsifsubmitted.py
@@ -0,0 +1,74 @@
+## TODO Need to look into whether or not this will handle groups
+
+import json
+import os
+from canvasapi import Canvas
+
+course_number = 21519
+assignment_number = 325713
+
+# Get Canvas API credentials
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+data = json.load(open(os.path.join(__location__, 'config.json')))
+
+api_key = data["canvas"]["access_token"]
+api_url = data["canvas"]["host"]
+
+
+# Initialize Canvas Object
+canvas = Canvas(api_url, api_key)
+
+
+# Get Course object
+course = canvas.get_course(course_number)
+
+assignment = course.get_assignment(assignment_number)
+
+# Get reference list of student names
+students = course.get_users(enrollment_type=['student'])
+student_list = []
+
+for student in students:
+    student_dict = {"id": student.id,
+                    "name": str(student.name)}
+    student_list.append(student_dict)
+
+# Give max points if student submitted. Based on
+# https://canvasapi.readthedocs.io/en/stable/examples.html#add-n-points-to-all-users-assignments
+
+points_awarded = assignment.points_possible
+
+submissions = assignment.get_submissions()
+
+count = sum(1 for submission in submissions if submission.score is None)
+
+print(assignment.name + " has " + str(count) + " submissions without a grade.")
+
+# Check if we should proceed with regrade
+while True:
+    yesno = input("Proceed with autograding? y/n \n")
+
+    if yesno == 'y':
+        break
+    if yesno == 'n':
+        exit()
+    if yesno != "y" or yesno != "n":
+        print("Please enter y or n")
+        continue
+
+# Do actual autograding
+for submission in submissions:
+
+    # Handle an unscored assignment by checking the `score` value for None
+
+    if submission.score is None:
+
+        for student in student_list:
+            if student['id'] == submission.user_id:
+                id_match = student
+
+        submission.edit(submission={'posted_grade': points_awarded})
+
+        print(id_match['name'] + " now has a score of " + str(submission.score))
