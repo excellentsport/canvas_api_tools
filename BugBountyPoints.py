4930efcbf5e3ab24b402636c07a42b7618c35fb6
George Beckham <35553484+excellentsport@users.noreply.github.com>
2024-02-12 10:51:55 -0800

    increased age of startdates, and fixed style issues

--- BugBountyPoints.py
+++ BugBountyPoints.py
@@ -2,12 +2,12 @@
 
 import json
 import os
-from canvasapi import Canvas
-from datetime import datetime, timezone
-import pyinputplus
 import re
+from datetime import datetime, timezone
+from canvasapi import Canvas
+import pyinputplus
 
-# TODO build this out with functions, classes etc so that it is more extensible than just the narrow function it current has.
+# TODO build this out with functions, classes etc to make more flexible
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -24,46 +24,53 @@ canvas = Canvas(api_url, api_key)
 # Get list of current courses I'm teaching
 courses = canvas.get_user(user_id).get_courses(enrollment_type="teacher", state='available')
 
-#filter out courses that are older than 120 days
-recent_courses = [course for course in courses if (datetime.now(timezone.utc) - course.created_at_date).days < 120]
+# filter out courses that are older than 120 days
+recent_courses = [course for course in courses if
+                  (datetime.now(timezone.utc) - course.created_at_date).days < 250]
 course_titles = [i.course_code for i in recent_courses]
 
-#select the correct course
-course_prompt_string = "Which course are you adding bug bounty points for?\n"
-for count, i in enumerate(course_titles): course_prompt_string += str(count+1) + ". " + i + "\n"
-response = pyinputplus.inputInt(prompt=course_prompt_string, min=1, max= len(course_titles))
+# select the correct course
+COURSE_PROMPT_STRING = "Which course are you adding bug bounty points for?\n"
+for count, i in enumerate(course_titles):
+    COURSE_PROMPT_STRING += str(count+1) + ". " + i + "\n"
+
+response = pyinputplus.inputInt(prompt=COURSE_PROMPT_STRING, min=1, max=len(course_titles))
 course = recent_courses[int(response)-1]
 
-#select the user to add points to
-student_prompt_string = "\n\nSelect a student from the following list:\n"
+# select the user to add points to
+STUDENT_PROMPT_STRING = "\n\nSelect a student from the following list:\n"
 student_dicts = []
-for count, student in enumerate(course.get_users(enrollment_type =['student'])):
-    dict = {"name":student.name, "id":student.id}
-    student_dicts.append(dict)
+for count, student in enumerate(course.get_users(enrollment_type=['student'])):
+    new_dict = {"name": student.name, "id": student.id}
+    student_dicts.append(new_dict)
 student_names = [student["name"] for student in student_dicts]
 
-response = pyinputplus.inputMenu(choices = student_names, prompt=student_prompt_string, numbered=True)
+response = pyinputplus.inputMenu(choices=student_names, prompt=STUDENT_PROMPT_STRING, numbered=True)
 
-sel_student_dict = None
+SEL_STUDENT_DICT = None
 for i in student_dicts:
     if i["name"] == response:
-        sel_student_dict = i
+        SEL_STUDENT_DICT = i
         break
 
 # get id for bug bounty assignment
 regex_bug = re.compile(".*bug.*", re.IGNORECASE)
-bug_assign_id = None
+BUG_ASSIGN_ID = None
 for i in course.get_assignments():
     if regex_bug.match(i.name):
-        bug_assign_id = i.id
+        BUG_ASSIGN_ID = i.id
 
-points_to_add = pyinputplus.inputInt(prompt = "\nHow many points should be added?\n", min=1, max=5)
+points_to_add = pyinputplus.inputInt(prompt="\nHow many points should be added?\n", min=1, max=5)
 
-assignment = course.get_assignment(bug_assign_id)
-submission = assignment.get_submission(sel_student_dict["id"])
+assignment = course.get_assignment(BUG_ASSIGN_ID)
+submission = assignment.get_submission(SEL_STUDENT_DICT["id"])
 
-print('\n' + sel_student_dict["name"] + "'s score for the Bug Bounty is " + str(submission.score) + ".")
-confirm = pyinputplus.inputYesNo(prompt = "Are you sure you wish to add " + str(points_to_add) + " points?\n")
+print('\n' + SEL_STUDENT_DICT["name"] +
+      "'s score for the Bug Bounty is " +
+      str(submission.score) + ".")
+confirm = pyinputplus.inputYesNo(prompt="Are you sure you wish to add " +
+                                        str(points_to_add) +
+                                        " points?\n")
 
 if confirm == "yes":
     if submission.score is not None:
