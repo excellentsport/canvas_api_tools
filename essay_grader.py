dd8e3eb2cfb417b94a64054a5eb7c7211f85400e
George Beckham <35553484+excellentsport@users.noreply.github.com>
2024-02-22 12:38:21 -0800

    start building essay_grader script

---
+++ essay_grader.py
@@ -0,0 +1,22 @@
+"""essay_grader.py - uses the Canvas API to download essay questions for quick grading at the command line"""
+
+import os
+from canvasapi import Canvas
+import canvas_lib
+
+API_KEY = os.environ['CANVAS_API_KEY']
+BETA_URL = os.environ['CANVAS_BETA_URL']
+PROD_URL = os.environ['CANVAS_PRODUCTION_URL']
+USER_ID = os.environ['CANVAS_USER_ID']
+
+canvas = Canvas(BETA_URL, API_KEY)
+
+classes = canvas_lib.get_current_courses(canvas, USER_ID)
+
+
+exit()
+
+assignments = course.get_assignments(bucket='ungraded')
+
+for assignment in assignments:
+    print(assignment)
